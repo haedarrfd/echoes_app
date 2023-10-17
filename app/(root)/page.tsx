9a74d29b99c291862ebaacbefa3bf1aa4d4a29eb
Tombstone2 <hroofid@gmail.com>
2023-10-17 10:16:32 +0700

    testing webhook fixed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -20,7 +20,7 @@ export default async function Home() {
               <EchoCard
                 key={post._id}
                 id={post._id}
-                currentUserId={user.id}
+                currentUserId={user?.id || ""}
                 parentId={post.parentId}
                 content={post.text}
                 author={post.author}
